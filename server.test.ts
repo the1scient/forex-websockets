21c8df6f5b74af2d1a3cb36838e7931a179ff948
the1scient <guifabbri04@hotmail.com>
2022-03-31 11:55:50 -0300

    make identation better

--- server.test.ts
+++ server.test.ts
@@ -4,7 +4,7 @@ import WebSocket from 'ws';
 describe('testing websocket server', () => {
     
     // create a test that tests a websocket server
-    it('should be able to connect to the websocket server', (done) => {
+    it('should be able to connect to the websocket server and retrieve API data', (done) => {
         // create a websocket client
         const ws = new WebSocket('ws://localhost:3001');
         
@@ -12,8 +12,6 @@ describe('testing websocket server', () => {
         ws.onopen = () => {
             ws.send('something');
         };
-        
-        
 
         // when the connection is open, expect the server to send us some data
         ws.onmessage = (event) => {
@@ -22,15 +20,11 @@ describe('testing websocket server', () => {
             done();
         };
 
-
         // when the connection is closed, expect status Code to be 1000
-            ws.onclose = (event) => {
-                expect(event.code).toBe(1000);
-                done();
-            };
-
-
-
+        ws.onclose = (event) => {
+            expect(event.code).toBe(1000);
+            done();
+        };
 
     });
     
